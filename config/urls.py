26bb4c6fe48ed3130657c3564a3c0562991b1eb0
lppavli <lp.pavli@gmail.com>
2022-09-13 18:00:36 +0300

    lesson_7

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
-
+import debug_toolbar
 from config import settings
 from mainapp import views
 
@@ -16,3 +16,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
