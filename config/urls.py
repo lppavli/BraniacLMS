78c7606d87675f542d79f1dc7b5b8180cf335b03
Liliya Pavliy <88142344+lppavli@users.noreply.github.com>
2022-09-15 09:03:51 +0300

    Lesson 8 (#3)
    * lesson_7
    
    * lesson_8

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
-
+import debug_toolbar
 from config import settings
 from mainapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("social_auth/", include("social_django.urls", namespace="social")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
     path("authapp/", include("authapp.urls", namespace="authapp")),
@@ -16,3 +17,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from mainapp import models as mainapp_models
 from django.utils.translation import gettext_lazy as _
+
+from mainapp import models as mainapp_models
 
 
 @admin.register(mainapp_models.News)
@@ -15,6 +16,7 @@ class LessonAdmin(admin.ModelAdmin):
     list_per_page = 5
     list_filter = ["course", "created", "deleted"]
     actions = ["mark_deleted"]
+    search_fields = ["title", "description"]
 
     def get_course_name(self, obj):
         return obj.course.name
@@ -25,3 +27,19 @@ class LessonAdmin(admin.ModelAdmin):
         queryset.update(deleted=True)
 
     mark_deleted.short_description = _("Mark deleted")
+
+
+@admin.register(mainapp_models.CourseTeachers)
+class CourseTeachersAdmin(admin.ModelAdmin):
+    list_display = ["id", "__str__", "get_courses"]
+    list_select_related = True
+
+    def get_courses(self, obj):
+        return ", ".join((i.name for i in obj.course.all()))
+
+    get_courses.short_description = _("Courses")
+
+
+@admin.register(mainapp_models.CourseFeedback)
+class CourseFeedbackAdmin(admin.ModelAdmin):
+    pass
